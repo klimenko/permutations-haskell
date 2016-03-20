b9554a37d3e97ab0e7d97d5a20d19b213f63bdd5
Dmitry Klimenko <github.com@klimenko.net>
2016-03-20 15:38:05 +0400

    Create main.hs

---
+++ main.hs
@@ -0,0 +1,16 @@
+module Main where
+
+perms zs = map (\z -> xi z zs) (perms' (ix zs)) where
+
+ ix xx = ix' [] 0 xx where
+  ix' r i [] = reverse r
+  ix' r i (x:xs) = ix' (i:r) (i+1) xs
+
+ xi ixs xs = xi' [] ixs xs where
+  xi' r [] _ = reverse r
+  xi' r (ix:ixs) xs = xi' ((geti ix xs):r) ixs xs 
+  geti 0 (x:xs) = x
+  geti i (x:xs) = geti (i-1) xs
+
+ perms' [] = [[]]
+ perms' xs = concatMap (\x -> concatMap (\y -> [x : y]) (perms' (filter (/=x) xs))) xs
